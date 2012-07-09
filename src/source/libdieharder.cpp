93fdfed68bc12c853a525d2a1c5719043de2a25d
Emery Berger <emery.berger@gmail.com>
2012-07-10 01:47:39 +0200

    Moved cruft to 'old' directory.

--- src/source/libdieharder.cpp
+++ src/source/libdieharder.cpp
@@ -18,7 +18,8 @@
 
 // The heap multiplier.
 
-enum { Numerator = 4, Denominator = 3 };
+enum { Numerator = 8, Denominator = 7 };
+
 // enum { Numerator = 2, Denominator = 1 };
 // enum { Numerator = 3, Denominator = 1 };
 // enum { Numerator = 4, Denominator = 1 };
@@ -46,14 +47,18 @@ enum { Numerator = 4, Denominator = 3 };
 
 class TheLargeHeap : public OneHeap<LargeHeap<MmapWrapper> > {};
 
-#include "debugheap.h"
 
-typedef ANSIWrapper<
+#include "../test/brokenheap.h"
+
+typedef
+//BrokenHeap<
+ ANSIWrapper<
   LockedHeap<PosixLockType,
 	     CombineHeap<DieHardHeap<Numerator, Denominator, 65536,
 				     (DIEHARD_DIEFAST == 1),
 				     (DIEHARD_DIEHARDER == 1)>,
 			 TheLargeHeap> > >
+//  >
 TheDieHardHeap;
 
 class TheCustomHeapType : public TheDieHardHeap {};
@@ -92,134 +97,3 @@ extern "C" {
   }
 
 }
-
-
-#if 0 // Disabled.
-
-/*************************  error-reporting functions ************************/
-
-#if !defined(_WIN32)
-
-extern "C" void reportOverflowError (void) {
-#if 0
-  static bool alertedAlready = false;
-  if (!alertedAlready) {
-    alertedAlready = true;
-    fprintf (stderr, "This application has performed an illegal action (an overflow).\n");
-  }
-#endif
-}
-
-extern "C" void reportDoubleFreeError (void) {
-#if 0
-  static bool alertedAlready = false;
-  if (!alertedAlready) {
-    alertedAlready = true;
-    fprintf (stderr, "This application has performed an illegal action (a double free),\nbut DieHard has prevented it from doing any harm.\n");
-  }
-#endif
-}
-
-extern "C" void reportInvalidFreeError (void) {
-#if 0
-  static bool alertedAlready = false;
-  if (!alertedAlready) {
-    alertedAlready = true;
-    fprintf (stderr, "This application has performed an illegal action (an invalid free),\nbut DieHard has prevented it from doing any harm.\n");
-  }
-#endif
-}
-
-// #include "heapshield.cpp"
-
-#endif
-
-/*************************  thread-intercept functions ************************/
-
-#if !defined(_WIN32)
-
-#include <pthread.h>
-#include <dlfcn.h>
-
-extern "C" {
-
-  typedef void * (*threadFunctionType) (void *);
-
-  typedef  
-  int (*pthread_create_function) (pthread_t *thread,
-				  const pthread_attr_t *attr,
-				  threadFunctionType start_routine,
-				  void *arg);
-
-  int pthread_create (pthread_t *thread,
-		      const pthread_attr_t *attr,
-		      void * (*start_routine) (void *),
-		      void * arg)
-#ifndef __SVR4
-    throw()
-#endif
-  {
-    static pthread_create_function real_pthread_create = NULL;
-    if (real_pthread_create == NULL) {
-      real_pthread_create = (pthread_create_function) dlsym (RTLD_NEXT, "pthread_create");
-    }
-    return (*real_pthread_create)(thread, attr, start_routine, arg);
-  }
-}
-#endif
-
-
-#if defined(_WIN32)
-#ifndef CUSTOM_DLLNAME
-#define CUSTOM_DLLNAME DllMain
-#endif
-
-#ifndef DIEHARD_PRE_ACTION
-#define DIEHARD_PRE_ACTION
-#endif
-
-#ifndef DIEHARD_POST_ACTION
-#define DIEHARD_POST_ACTION
-#endif
-#endif // _WIN32
-
-/*************************  default DLL initializer for Windows ************************/
-
-#if defined(_WIN32) && !defined(NO_WIN32_DLL)
-
-extern "C" {
-
-BOOL WINAPI CUSTOM_DLLNAME (HANDLE hinstDLL, DWORD fdwReason, LPVOID lpreserved)
-{
-  char buf[255];
-
-  switch (fdwReason) {
-
-  case DLL_PROCESS_ATTACH:
-    sprintf (buf, "Using the DieHarder application-hardening system (Windows DLL, version %d.%d).\nCopyright (C) 2011 Emery Berger, University of Massachusetts Amherst.\n", VERSION_MAJOR_NUMBER, VERSION_MINOR_NUMBER);
-    fprintf (stderr, buf);
-    DIEHARD_PRE_ACTION;
-    break;
-
-  case DLL_PROCESS_DETACH:
-    DIEHARD_POST_ACTION;
-    break;
-
-  case DLL_THREAD_ATTACH:
-    break;
-
-  case DLL_THREAD_DETACH:
-    break;
-
-  default:
-    return TRUE;
-  }
-  
-  return TRUE;
-}
-
-}
-
-#endif // WIN32
-
-#endif
