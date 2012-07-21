769f09fd6fc08aa4f44dc9c6d7ac1333851be6e8
Emery Berger <emery.berger@gmail.com>
2012-07-22 00:14:42 +0200

    Added warm-up.

--- src/include/generate-bitstream.cpp
+++ src/include/generate-bitstream.cpp
@@ -18,15 +18,28 @@ enum { HI_INSIG_BITS = 5};
 int
 main()
 {
+  char ** arr = new char *[1000000];
+
+#if 1
+  // Warm-up period:
+  for (int k = 0; k < 100; k++) {
+    for (int j = 0; j < 1000000; j++) {
+      arr[j] = new char[8];
+    }
+    for (int j = 0; j < 1000000; j++) {
+      delete [] arr[j];
+    }
+  }
+#endif
   const int count = 1000;
-  char * arr[count];
   for (int j = 0; j < 1000; j++) {
     for (int i = 0; i < count; i++) {
-      long rnd = lrand48() & ~((1<<HI_INSIG_BITS) - 1);
       arr[i] = new char[8];
-      //      std::bitset<16> binary ((size_t) arr[i] >> 4);
+
+      long rnd = lrand48() & ~((1<<HI_INSIG_BITS) - 1);
       long val = ((size_t) arr[i] >> LO_INSIG_BITS) & ((1<<HI_INSIG_BITS) - 1);
       std::bitset<16> binary (rnd + val);
+
       cout << binary << endl;
     }
     
