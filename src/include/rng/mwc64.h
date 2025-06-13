655395add78bd8f5b3c3db3871acddf77f21f530
Emery Berger <emery.berger@gmail.com>
2025-06-13 16:43:42 -0400

    Revert "Added seed management"

--- src/include/rng/mwc64.h
+++ src/include/rng/mwc64.h
@@ -11,6 +11,14 @@ class MWC64 {
 
   uint64_t _x, _c, _t;
 
+  void init (uint64_t seed1, uint64_t seed2)
+  {
+    _x = seed1;
+    _x <<= 32;
+    _x += seed2;
+    _c = 123456123456123456ULL;
+  }
+
   inline uint64_t MWC() {
     _t = (_x << 58) + _c;
     _c = _x >> 6;
@@ -21,27 +29,18 @@ class MWC64 {
 
 public:
   
-  void seed(uint64_t seed1, uint64_t seed2)
-  {
-    _x = seed1;
-    _x <<= 32;
-    _x += seed2;
-    _c = 123456123456123456ULL;
-  }
-
-  
   MWC64()
   {
     auto a = RealRandomValue::value();
     auto b = RealRandomValue::value();
-    seed(a, b);
+    init (a, b);
   }
-
+  
   MWC64 (uint64_t seed1, uint64_t seed2)
   {
-    seed(seed1, seed2);
+    init (seed1, seed2);
   }
-
+  
   inline uint64_t next()
   {
     return MWC();
--- src/include/rng/realrandomvalue.h
+++ src/include/rng/realrandomvalue.h
@@ -13,12 +13,8 @@
 
 #include <fcntl.h>
 #include <unistd.h>
-#include <cstdlib>
-#include <sys/stat.h>
 
 #include <random>
-
-#include "printf.h"
 
 /**
  * @class RealRandomValue
@@ -32,32 +28,9 @@ public:
   {}
 
   static unsigned int value() {
-    // If the environment variable DIEHARD_RANDOM_SEED is set, use its
-    // value as the given seed.
-    const char* varName = "DIEHARD_RANDOM_SEED";
-    char* varValue = getenv(varName);
-    if (varValue) {
-      auto seed = atoi(varValue);
-      return seed;
-    }
     int fd = open("/dev/urandom", O_RDONLY);
     unsigned int buf;
     read(fd, (void *)&buf, sizeof(buf));
-    // If the environment variable DIEHARD_OUTPUT_SEED_FILE is set,
-    // write the seed to it.
-    {
-      const char* varName = "DIEHARD_OUTPUT_SEED_FILE";
-      char* filename = getenv(varName);
-      if (filename) {
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd >= 0) {
-	  char str[256];
-	  snprintf_(str, sizeof(str), "%u\n", buf);
-	  write(fd, str, strlen(str));
-	  close(fd);
-	}
-      }
-    }
     return buf;
   }
 };
